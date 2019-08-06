f2530501b7fdd7020ee31e3cec10796843b9674c
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 18:59:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,11 @@
 def new_hash
   # return an empty hash
-  new_hash = Hash.new
+  hash1 = Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = {
+  hash2 = {
     :key1 => "value1"
   }
 end
