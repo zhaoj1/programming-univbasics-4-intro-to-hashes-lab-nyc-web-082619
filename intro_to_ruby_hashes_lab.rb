e26770e26f0014db2e35ac2b52e02c5fadd8a5d8
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 19:14:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   if hash[key]
-    puts hash[key]
+    hash[key] += 1
   else
     hash[key] = 1
   end
