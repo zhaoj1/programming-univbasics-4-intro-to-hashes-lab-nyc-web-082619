c903833683c1320166d727fd4f35c2b6abed7663
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 19:07:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 def id_generator
   # return a hash with a key :id assigned to positive integer
   generator_hash = {
-    :symbol1 => 1
+    :symbol1 => 1.integer
   }
 end
 
@@ -34,7 +34,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   if hash[key]
-    hash[key] += 1
+    hash[key] = 1
   end
 end
 
@@ -43,7 +43,7 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   if hash[key]
-    hash[key] +=1
+    puts hash[key]
   else
     hash[key] = 1
   end
